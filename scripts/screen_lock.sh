b2b751f98d02450d5d0152f943241642d0b36ad7
D-LopGar03 <damianlopez613@gmail.com>
2025-03-02 10:39:24 -0500

    Crear archivo para modificar el fondo de swaylock

---
+++ scripts/screen_lock.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+wallpaper_dir="/usr/share/backgrounds/"
+
+random_dir=$(find "$wallpaper_dir" -type d | shuf -n 1)
+
+final_dir="$random_dir"
+
+wallpaper=$(find "$final_dir" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) | shuf -n 1)
+
+swaylock -f -i $wallpaper
